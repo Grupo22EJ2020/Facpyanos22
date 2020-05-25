0577a5e9f5095cbcc0a003a1095db0b7f12c9eba
Cesar Lopez <zepol14117@gmail.com>
2020-05-24 22:26:58 -0500

    Clase video lista

--- video.py
+++ video.py
@@ -56,4 +56,65 @@ class Video:
             if video == videoelim:
                 lista.remove(video)
                 print("Su video ha sido removido.")
-                
+list = []
+video1 = Video("1121", "python", "0", 51220)
+list.append(video1)
+menuOpciones = "1.- Ver info de todos los videos.\n2.- Elegir un video.\n3.- Agregar un video.\n4.- Eliminar un video.\n5.- Modificar los datos de un video.\n6.- Salir"    
+submenuOpciones = "1.- Ver datos del video."
+Minimenu = 0
+
+Video.verLista(list)
+clave = input("Cual es la id de tu video?: ")
+for video in list:
+    if clave == video.idVideo:
+        print(submenuOpciones)
+        submenu = input("Que desea hacer?: ")
+        if submenu == "1":
+            print(video.datos())
+
+   
+print(menuOpciones)
+Minimenu = int(input("Que desea hacer?: "))
+while Minimenu != 6:
+    if Minimenu == 1:
+        Video.verLista(list)
+        print(menuOpciones)
+        Minimenu = int(input("Que desea hacer?: "))
+    if Minimenu == 2:
+        Video.verLista(list)
+        clave = input("Cual es la id de tu video?: ")
+        for video in list:
+            if clave == video.idVideo:
+                print(submenuOpciones)
+                submenu = input("Que desea hacer?: ")
+                if submenu == "1":
+                    print(video.datos)
+        print(menuOpciones)
+        Minimenu = int(input("Que desea hacer?: "))
+    if Minimenu == 3:
+        idVideo = input("Dame la id del video: ")
+        nombre = input("Dame el nombre del video: ")
+        fechapublicacion = input("Dame la fecha del video: ")
+        url = input("Dame el url del video: ")
+        Video.agregarVideo(idVideo,nombre,fechapublicacion,url,list)
+        print(menuOpciones)
+        Minimenu = int(input("Que desea hacer?: "))
+    if Minimenu == 4:
+        Video.eliminarVideo(list)
+        print(menuOpciones)
+        Minimenu = int(input("Que desea hacer?: "))
+    if Minimenu == 5:
+        Video.verLista
+        clave = input("Cual es la clave del video que desea modificar?:")
+        for video in list:
+            if clave == video.idVideo:
+                submenu == input("Quieres cambiarle el nombre, la fecha o el url?")
+                if submenu == "titulo":
+                    video.modificarnombre(video)
+                elif submenu == "fechapublicacion":
+                    video.modificarfecha(video)
+                elif submenu == "url":
+                    video.modificarURL(video)
+        print(menuOpciones)
+        Minimenu = int(input("Que desea hacer?: "))
+print("El programa ha concluido con éxito")
